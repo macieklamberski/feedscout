a77d209fd3c8ed7626792c628ce95a809ae2a336
Maciej Lamberski <maciej@lamberski.com>
2025-12-09 17:26:13 +0100

    chore: Simplify the export file for Platform method

--- src/exports/platform.ts
+++ src/exports/platform.ts
@@ -1,10 +1,5 @@
-// Platform handlers for feed discovery.
-
-// Platform discovery function.
 export { discoverUrisFromPlatform } from '../common/uris/platform/index.js'
-// Types.
 export type { PlatformHandler, PlatformMethodOptions } from '../common/uris/platform/types.js'
-// Default platform options.
 export { defaultPlatformOptions } from '../feeds/defaults.js'
 export { githubHandler } from '../feeds/platform/handlers/github.js'
 export { redditHandler } from '../feeds/platform/handlers/reddit.js'
