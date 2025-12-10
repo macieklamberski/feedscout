4600174c6395befb482f6bff3ae2949d6b4d59e8
Maciej Lamberski <maciej@lamberski.com>
2025-12-10 19:13:59 +0100

    feat: Add wordpress platform handler

---
+++ src/feeds/platform/handlers/wordpress.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it } from 'bun:test'
+import { wordpressHandler } from './wordpress.js'
+
+describe('wordpressHandler', () => {
+  describe('match', () => {
+    const cases = [
+      ['https://example.wordpress.com', true],
+      ['https://blog.example.wordpress.com', true],
+      ['https://wordpress.com', false],
+      ['https://example.com', false],
+    ] as const
+
+    it.each(cases)('%s -> %s', (url, expected) => {
+      expect(wordpressHandler.match(url)).toBe(expected)
+    })
+  })
+
+  describe('resolve', () => {
+    it('should return feed URL for blog', () => {
+      const value = 'https://example.wordpress.com'
+      const expected = ['https://example.wordpress.com/feed/']
+
+      expect(wordpressHandler.resolve(value)).toEqual(expected)
+    })
+
+    it('should return feed URL regardless of path', () => {
+      const value = 'https://blog.wordpress.com/2024/01/01/some-post/'
+      const expected = ['https://blog.wordpress.com/feed/']
+
+      expect(wordpressHandler.resolve(value)).toEqual(expected)
+    })
+  })
+})
---
+++ src/feeds/platform/handlers/wordpress.ts
@@ -0,0 +1,14 @@
+import type { PlatformHandler } from '../../../common/uris/platform/types.js'
+import { isSubdomainOf } from '../../../common/utils.js'
+
+export const wordpressHandler: PlatformHandler = {
+  match: (url) => {
+    return isSubdomainOf(url, 'wordpress.com')
+  },
+
+  resolve: (url) => {
+    const { origin } = new URL(url)
+
+    return [`${origin}/feed/`]
+  },
+}
