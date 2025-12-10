0e0911382cacf9777933dfdf3e0687589be072ae
Maciej Lamberski <maciej@lamberski.com>
2025-12-10 20:01:21 +0100

    feat: Add GitLab platform handler

--- src/feeds/defaults.ts
+++ src/feeds/defaults.ts
@@ -6,6 +6,7 @@ import type { PlatformMethodOptions } from '../common/uris/platform/types.js'
 import { blogspotHandler } from './platform/handlers/blogspot.js'
 import { blueskyHandler } from './platform/handlers/bluesky.js'
 import { githubHandler } from './platform/handlers/github.js'
+import { gitlabHandler } from './platform/handlers/gitlab.js'
 import { redditHandler } from './platform/handlers/reddit.js'
 import { substackHandler } from './platform/handlers/substack.js'
 import { tumblrHandler } from './platform/handlers/tumblr.js'
@@ -100,6 +101,7 @@ export const defaultPlatformOptions: Omit<PlatformMethodOptions, 'baseUrl'> = {
     blogspotHandler,
     blueskyHandler,
     githubHandler,
+    gitlabHandler,
     redditHandler,
     substackHandler,
     tumblrHandler,
