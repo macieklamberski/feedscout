b924d313b27a5864e410813802e1817c8e0e2ca0
Maciej Lamberski <maciej@lamberski.com>
2025-12-10 19:24:55 +0100

    feat: Add bluesky platform handler

--- src/feeds/index.test.ts
+++ src/feeds/index.test.ts
@@ -1115,10 +1115,10 @@ describe('discoverFeeds', () => {
 })
 
 describe('defaultPlatformOptions', () => {
-  it('should contain all 7 platform handlers', () => {
+  it('should contain all 8 platform handlers', () => {
     const value = defaultPlatformOptions.handlers.length
 
-    expect(value).toBe(7)
+    expect(value).toBe(8)
   })
 
   it('should contain handler that matches GitHub URLs', () => {
