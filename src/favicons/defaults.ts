72564d079cdee7ca9e2849f27cce8945eb4ae41d
Maciej Lamberski <maciej@lamberski.com>
2026-03-26 20:05:01 +0100

    feat: Add Dev.to favicon handler

--- src/favicons/defaults.ts
+++ src/favicons/defaults.ts
@@ -8,6 +8,7 @@ import { omitEmpty } from '../common/utils.js'
 import { blueskyHandler } from './platform/handlers/bluesky.js'
 import { codebergHandler } from './platform/handlers/codeberg.js'
 import { deviantartHandler } from './platform/handlers/deviantart.js'
+import { devtoHandler } from './platform/handlers/devto.js'
 import { githubHandler } from './platform/handlers/github.js'
 import { githubGistHandler } from './platform/handlers/githubGist.js'
 import { lobstersHandler } from './platform/handlers/lobsters.js'
@@ -74,5 +75,6 @@ export const defaultPlatformOptions: Omit<PlatformMethodOptions, 'baseUrl'> = {
     lobstersHandler,
     sourceforgeHandler,
     deviantartHandler,
+    devtoHandler,
   ],
 }
