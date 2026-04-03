e750dfddb58974e6a43e91a83fe089c51983ef25
Maciej Lamberski <maciej@lamberski.com>
2026-04-03 12:28:46 +0200

    feat: Add Substack profile page feed discovery

--- src/feeds/platform/handlers/substack.ts
+++ src/feeds/platform/handlers/substack.ts
@@ -1,14 +1,37 @@
 import type { PlatformHandler } from '../../../common/uris/platform/types.js'
-import { composeHint, isSubdomainOf } from '../../../common/utils.js'
+import { composeHint, isHostOf, isSubdomainOf } from '../../../common/utils.js'
+
+const profilePattern = /^\/@([\w-]+)/
 
 export const substackHandler: PlatformHandler = {
   match: (url) => {
-    return isSubdomainOf(url, 'substack.com')
+    if (isSubdomainOf(url, 'substack.com')) {
+      return true
+    }
+
+    try {
+      return isHostOf(url, 'substack.com') && profilePattern.test(new URL(url).pathname)
+    } catch {}
+
+    return false
   },
 
   resolve: (url) => {
-    const { origin } = new URL(url)
+    const parsed = new URL(url)
 
-    return [{ uri: `${origin}/feed`, hint: composeHint('substack:newsletter') }]
+    if (isHostOf(url, 'substack.com')) {
+      const match = parsed.pathname.match(profilePattern)
+
+      if (match?.[1]) {
+        return [
+          {
+            uri: `https://${match[1]}.substack.com/feed`,
+            hint: composeHint('substack:newsletter'),
+          },
+        ]
+      }
+    }
+
+    return [{ uri: `${parsed.origin}/feed`, hint: composeHint('substack:newsletter') }]
   },
 }
