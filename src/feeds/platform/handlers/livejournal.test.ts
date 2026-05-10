30ce1d355854aa4ad54cb80c379cd7a77c082f33
Maciej Lamberski <maciej@lamberski.com>
2026-05-10 18:20:25 +0200

    fix: Canonicalise LiveJournal alt-host paths and add tag/userpics feeds

--- src/feeds/platform/handlers/livejournal.test.ts
+++ src/feeds/platform/handlers/livejournal.test.ts
@@ -6,6 +6,14 @@ describe('livejournalHandler', () => {
     const cases = [
       ['https://ohnotheydidnt.livejournal.com', true],
       ['https://blog.example.livejournal.com', true],
+      ['https://www.livejournal.com/users/news', true],
+      ['https://www.livejournal.com/~news', true],
+      ['https://users.livejournal.com/news', true],
+      ['https://community.livejournal.com/ohnotheydidnt', true],
+      ['https://www.livejournal.com', false],
+      ['https://users.livejournal.com', false],
+      ['https://community.livejournal.com', false],
+      ['https://syndicated.livejournal.com', false],
       ['https://livejournal.com', false],
       ['https://example.com', false],
     ] as const
@@ -20,7 +28,7 @@ describe('livejournalHandler', () => {
   })
 
   describe('resolve', () => {
-    it('should return RSS and Atom feeds for blog', () => {
+    it('should return RSS, Atom, and userpics feeds for blog', () => {
       const value = 'https://ohnotheydidnt.livejournal.com'
       const expected = [
         {
@@ -30,6 +38,10 @@ describe('livejournalHandler', () => {
         {
           uri: 'https://ohnotheydidnt.livejournal.com/data/atom',
           hint: { key: 'livejournal:posts-atom', label: 'Posts (Atom)' },
+        },
+        {
+          uri: 'https://ohnotheydidnt.livejournal.com/data/userpics',
+          hint: { key: 'livejournal:userpics-atom', label: 'Userpics (Atom)' },
         },
       ]
 
@@ -46,6 +58,98 @@ describe('livejournalHandler', () => {
         {
           uri: 'https://ohnotheydidnt.livejournal.com/data/atom',
           hint: { key: 'livejournal:posts-atom', label: 'Posts (Atom)' },
+        },
+        {
+          uri: 'https://ohnotheydidnt.livejournal.com/data/userpics',
+          hint: { key: 'livejournal:userpics-atom', label: 'Userpics (Atom)' },
+        },
+      ]
+
+      expect(livejournalHandler.resolve(value)).toEqual(expected)
+    })
+
+    it('should add tag-filtered feeds for /tag/ paths', () => {
+      const value = 'https://ohnotheydidnt.livejournal.com/tag/television'
+      const expected = [
+        {
+          uri: 'https://ohnotheydidnt.livejournal.com/data/rss?tag=television',
+          hint: { key: 'livejournal:posts-tag-rss', label: 'Tag (RSS)' },
+        },
+        {
+          uri: 'https://ohnotheydidnt.livejournal.com/data/atom?tag=television',
+          hint: { key: 'livejournal:posts-tag-atom', label: 'Tag (Atom)' },
+        },
+        {
+          uri: 'https://ohnotheydidnt.livejournal.com/data/rss',
+          hint: { key: 'livejournal:posts-rss', label: 'Posts (RSS)' },
+        },
+        {
+          uri: 'https://ohnotheydidnt.livejournal.com/data/atom',
+          hint: { key: 'livejournal:posts-atom', label: 'Posts (Atom)' },
+        },
+        {
+          uri: 'https://ohnotheydidnt.livejournal.com/data/userpics',
+          hint: { key: 'livejournal:userpics-atom', label: 'Userpics (Atom)' },
+        },
+      ]
+
+      expect(livejournalHandler.resolve(value)).toEqual(expected)
+    })
+
+    it('should canonicalise www.livejournal.com/users/{user} to subdomain', () => {
+      const value = 'https://www.livejournal.com/users/news'
+      const expected = [
+        {
+          uri: 'https://news.livejournal.com/data/rss',
+          hint: { key: 'livejournal:posts-rss', label: 'Posts (RSS)' },
+        },
+        {
+          uri: 'https://news.livejournal.com/data/atom',
+          hint: { key: 'livejournal:posts-atom', label: 'Posts (Atom)' },
+        },
+        {
+          uri: 'https://news.livejournal.com/data/userpics',
+          hint: { key: 'livejournal:userpics-atom', label: 'Userpics (Atom)' },
+        },
+      ]
+
+      expect(livejournalHandler.resolve(value)).toEqual(expected)
+    })
+
+    it('should canonicalise users.livejournal.com/{user} legacy host', () => {
+      const value = 'https://users.livejournal.com/news'
+      const expected = [
+        {
+          uri: 'https://news.livejournal.com/data/rss',
+          hint: { key: 'livejournal:posts-rss', label: 'Posts (RSS)' },
+        },
+        {
+          uri: 'https://news.livejournal.com/data/atom',
+          hint: { key: 'livejournal:posts-atom', label: 'Posts (Atom)' },
+        },
+        {
+          uri: 'https://news.livejournal.com/data/userpics',
+          hint: { key: 'livejournal:userpics-atom', label: 'Userpics (Atom)' },
+        },
+      ]
+
+      expect(livejournalHandler.resolve(value)).toEqual(expected)
+    })
+
+    it('should canonicalise community.livejournal.com/{user} legacy host', () => {
+      const value = 'https://community.livejournal.com/ohnotheydidnt'
+      const expected = [
+        {
+          uri: 'https://ohnotheydidnt.livejournal.com/data/rss',
+          hint: { key: 'livejournal:posts-rss', label: 'Posts (RSS)' },
+        },
+        {
+          uri: 'https://ohnotheydidnt.livejournal.com/data/atom',
+          hint: { key: 'livejournal:posts-atom', label: 'Posts (Atom)' },
+        },
+        {
+          uri: 'https://ohnotheydidnt.livejournal.com/data/userpics',
+          hint: { key: 'livejournal:userpics-atom', label: 'Userpics (Atom)' },
         },
       ]
 
