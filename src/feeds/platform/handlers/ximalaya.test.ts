0f81b7e4d53fd89d2d5b1cbf5765c13b716c6a4d
Maciej Lamberski <maciej@lamberski.com>
2026-05-10 18:42:22 +0200

    feat: Match legacy /{userid}/album/{id} on Ximalaya handler

--- src/feeds/platform/handlers/ximalaya.test.ts
+++ src/feeds/platform/handlers/ximalaya.test.ts
@@ -60,5 +60,17 @@ describe('ximalayaHandler', () => {
 
       expect(ximalayaHandler.resolve(value)).toEqual([])
     })
+
+    it('should match legacy /{userid}/album/{id} form', () => {
+      const value = 'https://www.ximalaya.com/61425525/album/6912905'
+      const expected = [
+        {
+          uri: 'https://www.ximalaya.com/album/6912905.xml',
+          hint: { key: 'ximalaya:album', label: 'Album' },
+        },
+      ]
+
+      expect(ximalayaHandler.resolve(value)).toEqual(expected)
+    })
   })
 })
